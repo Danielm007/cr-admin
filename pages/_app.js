50a9b8d37fd3040e68b2319232e4e4ca6cb8777a
Daniel <danielmorocho07@outlook.com>
2021-11-15 17:01:26 -0500

    Font family added

--- pages/_app.js
+++ pages/_app.js
@@ -3,19 +3,10 @@ import "antd/dist/antd.css";
 import { ContextProvider } from "../context/Context";
 import { Header } from "../components/ui/Header";
 import { Footer } from "../components/ui/Footer";
-import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ContextProvider>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
       <Header />
       <Component {...pageProps} />
       <Footer />
